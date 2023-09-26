b9e2f207f0b302df23905ba4f85e5d7f40c32e35
sudharshan1234 <sudarchakra2000@gmail.com>
2023-09-25 20:15:04 -0400

    added jdoc for InputValidation.java

--- src/InputValidation.java
+++ src/InputValidation.java
@@ -1,5 +1,16 @@
-
+/**
+ * The InputValidation class provides methods to validate user input for the
+ * Expense Tracker application.
+ */
 public class InputValidation {
+
+	/**
+	 * Validates if the provided amount is within the valid range of 0 to 1000.
+	 *
+	 * @param amountField The input representing the amount.
+	 * @throws InvalidAmountException If the input is not a valid amount (outside
+	 *                                the range 0 to 1000).
+	 */
 	public void isAmountValid(String amountField) throws InvalidAmountException {
 		try {
 			double amount = Double.parseDouble(amountField);
@@ -14,6 +25,12 @@ public class InputValidation {
 
 	}
 
+	/**
+	 * Validates if the provided category is one of the predefined valid categories.
+	 *
+	 * @param category The input representing the category.
+	 * @throws InvalidCategoryException If the input is not a valid category.
+	 */
 	public void isCategoryValid(String category) throws InvalidCategoryException {
 		String[] validCategories = { "food", "travel", "bills", "entertainment", "other" };
 		for (String validCategory : validCategories) {
