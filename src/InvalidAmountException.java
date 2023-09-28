6f75df99e9b5a2fb71bf28511b3e329a25fb1ac8
sudharshan1234 <sudarchakra2000@gmail.com>
2023-09-27 20:26:37 -0400

    added jdoc comments for Exception classes

--- src/InvalidAmountException.java
+++ src/InvalidAmountException.java
@@ -1,6 +1,17 @@
 
+/**
+ * The InvalidAmountException class is a custom exception that extends the
+ * Exception class.
+ * It is thrown when an invalid amount is encountered, providing a specific
+ * error message.
+ */
 public class InvalidAmountException extends Exception {
 
+	/**
+	 * Constructs a new InvalidAmountException with the specified error message.
+	 *
+	 * @param message The error message associated with the exception.
+	 */
 	public InvalidAmountException(String message) {
 		super(message);
 	}
